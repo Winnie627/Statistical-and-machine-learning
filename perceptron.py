9855572fd60137e1079557fa7e55c13520ef69d7
Winnie627 <15801729150@163.com>
2020-03-30 12:23:01 +0800

    增加注释

--- perceptron.py
+++ perceptron.py
@@ -16,9 +16,10 @@ Y = np.array([y1, y2, y3])
 
 max_iter = 100
 n = 0
-w = np.zeros(X.shape[1]+1)
-b = []
+w = np.zeros(X.shape[1]+1)  # add b to vector w
 correct_count = 0
+
+# 随机梯度下降法迭代
 while n < max_iter:
     index = random.randint(0, Y.shape[0]-1)
     x = np.hstack([X[index], 1])
@@ -33,8 +34,12 @@ while n < max_iter:
     n += 1
     if True:
         print(n)
+print(w)  # 选取出模型参数w,b
 
+# 给定一组数据，即输入仍为X，输出均为1，判断数据点的ture/false
 X = np.hstack([X, np.ones(X.shape[0]).reshape((-1, 1))])
+
+# predict the given 3 group data,1 for ture, -1 for false
 rst = np.array([1 if rst else -1 for rst in np.dot(X, w) > 0])
-print(rst)
+# >>[1 1 -1]
 
